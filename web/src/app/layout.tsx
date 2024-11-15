f909694c61939d1821ea5a627995cac7079a0543
Azdin Mediri <a.elmediri@gmail.com>
2024-11-15 20:58:55 +0100

    test branch

--- web/src/app/layout.tsx
+++ web/src/app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        hello
         {children}
       </body>
     </html>
